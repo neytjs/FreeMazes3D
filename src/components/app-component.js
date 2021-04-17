aa523ccacb7722ec25f2677ee521caf4d52c29d3
neytjs <35025868+neytjs@users.noreply.github.com>
2021-04-17 03:14:46 -0500

    Create app-component.js

---
+++ src/components/app-component.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+import Canvas from './canvas-component';
+
+class App extends Component {
+  render() {
+    return (
+      <Canvas></Canvas>
+    )
+  }  
+}
+
+export default App;
