ed9ff5161e7c25c5e1717922a9dc1fb2b62533f9
neytjs <35025868+neytjs@users.noreply.github.com>
2021-04-17 03:13:10 -0500

    Create app.js

---
+++ src/app.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/app-component';
+
+ReactDOM.render(
+    <App></App>
+  ,document.getElementById('output')
+);
