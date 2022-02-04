7bb5419407f14cb49cb00b8c5000e6ed1a5a0546
neytjs <35025868+neytjs@users.noreply.github.com>
2022-02-03 22:48:38 -0600

    Add files via upload

--- src/components/gui/gui_key_controls.js
+++ src/components/gui/gui_key_controls.js
@@ -3,7 +3,8 @@ function showKeyControls(global_keys) {
   document.getElementById("keys_activate").innerHTML = global_keys.action_key.new_key.toUpperCase();
   document.getElementById("keys_toggleleft").innerHTML = global_keys.toggle_left.new_key.toUpperCase();
   document.getElementById("keys_toggleright").innerHTML = global_keys.toggle_right.new_key.toUpperCase();
-  document.getElementById("keys_fps").innerHTML = global_keys.show_fps.new_key.toUpperCase();
+  document.getElementById("keys_fps").innerHTML = global_keys.show_fps.new_key.toUpperCase();   
+  document.getElementById("keys_weapon").innerHTML = global_keys.use_weapon.new_key.toUpperCase();
 }
 
 export {showKeyControls};
